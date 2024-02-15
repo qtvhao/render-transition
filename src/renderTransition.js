4dee88c6e6fab6ead58bb138d760b3fe4fb2c759
Your Name <you@example.com>
2024-02-15 07:26:49 +0000

    Refactor renderTransition.js to use random font styles and vibrant dark colors

--- src/renderTransition.js
+++ src/renderTransition.js
@@ -1,5 +1,67 @@
 let fs = require('fs');
 let sharp = require('sharp');
+function getRandomFontStyle() {
+    var fontStyles = [
+      'MediumItalic', 'Italic', 'Light', 'BlackItalic', 'Alternates-BoldItalic', 'Alternates-Bold', 'Alternates-LightItalic', 'Alternates-MediumItalic', 'SemiBold', 'Thin', 'Alternates-ExtraBold', 'Black', 'Regular', 'SemiBoldItalic', 'Alternates-ExtraBoldItalic', 'Bold', 'ExtraBoldItalic', 'Alternates-ExtraLightItalic', 'Alternates-Black', 'Alternates-ThinItalic', 'Alternates-ExtraLight', 'Medium', 'ExtraBold', 'Alternates-SemiBold', 'Alternates-BlackItalic', 'LightItalic', 'Alternates-SemiBoldItalic', 'Alternates-Light', 'ExtraLight', 'ThinItalic', 'ExtraLightItalic', 'BoldItalic', 'Alternates-Thin', 'Alternates-Medium', 'Alternates-Regular', 'Alternates-Italic',
+    ].filter(font => {
+      return !font.includes('Alternates') && !font.includes('Italic') && !font.includes('Light')
+    } )
+// 
+// Montserrat-MediumItalic.otf
+// Montserrat-Italic.otf
+// Montserrat-Light.otf
+// Montserrat-BlackItalic.otf
+// fonts.scale
+// MontserratAlternates-BoldItalic.otf
+// MontserratAlternates-Bold.otf
+// MontserratAlternates-LightItalic.otf
+// MontserratAlternates-MediumItalic.otf
+// Montserrat-SemiBold.otf
+// Montserrat-Thin.otf
+// MontserratAlternates-ExtraBold.otf
+// Montserrat-Black.otf
+// Montserrat-Regular.otf
+// Montserrat-SemiBoldItalic.otf
+// MontserratAlternates-ExtraBoldItalic.otf
+// Montserrat-Bold.otf
+// Montserrat-ExtraBoldItalic.otf
+// MontserratAlternates-ExtraLightItalic.otf
+// MontserratAlternates-Black.otf
+// MontserratAlternates-ThinItalic.otf
+// MontserratAlternates-ExtraLight.otf
+// Montserrat-Medium.otf
+// Montserrat-ExtraBold.otf
+// MontserratAlternates-SemiBold.otf
+// MontserratAlternates-BlackItalic.otf
+// Montserrat-LightItalic.otf
+// MontserratAlternates-SemiBoldItalic.otf
+// MontserratAlternates-Light.otf
+// Montserrat-ExtraLight.otf
+// Montserrat-ThinItalic.otf
+// Montserrat-ExtraLightItalic.otf
+// Montserrat-BoldItalic.otf
+// MontserratAlternates-Thin.otf
+// MontserratAlternates-Medium.otf
+// MontserratAlternates-Regular.otf
+// MontserratAlternates-Italic.otf
+// fonts.dir
+  
+    var fontStyle = fontStyles[Math.floor(Math.random() * fontStyles.length)];
+    return "Montserrat-" + fontStyle;
+}
+var vibrantDarkColors = [
+    "004895",
+    "1A237E",
+    "283593",
+    "303F9F",
+    "4A148C",
+    "512DA8",
+    "673AB7",
+    "7B1FA2",
+    "880E4F",
+    "9C27B0",
+  ];
+  
 let renderTransition = async (translated, transitionImage) => {
     console.log("Rendering transition")
     console.log("Translated:", translated)
@@ -11,10 +73,11 @@ let renderTransition = async (translated, transitionImage) => {
         await sharp(transitionImage)
             .resize(3840, 2160)
             .toFile(resizedTransitionImage)
-        let font = 'Montserrat'
+        let font = getRandomFontStyle();
+        var color = vibrantDarkColors[Math.floor(Math.random() * vibrantDarkColors.length)];
         await sharp({
             text: {
-                text: translated,
+                text: '<span foreground="#' + color + '">' + translated + '</span>',
                 //   resolution is 4k
                 width: 3840 * 0.8,
                 height: 2160 * 0.8,
@@ -30,7 +93,7 @@ let renderTransition = async (translated, transitionImage) => {
                     gravity: 'center'
                 }
             ])
-            .toFile('/app/storage/images/transition-' + font + '.jpg')
+            .toFile('/app/storage/images/transition-' + font + '-' + color + '.jpg')
     } else {
         console.log("Transition image does not exist")
     }
