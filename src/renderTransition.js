116d2eecb956e9f0d565cfcaee95849e1ac4fc5f
Your Name <you@example.com>
2024-02-15 02:25:24 +0000

    Refactor renderTransition function to check if transition image exists

--- src/renderTransition.js
+++ src/renderTransition.js
@@ -1,7 +1,14 @@
+let fs = require('fs');
 let renderTransition = async (translated, transitionImage) => {
     console.log("Rendering transition")
-    console.log("Translated: ", translated)
-    console.log("Transition Image: ", transitionImage)
+    console.log("Translated:", translated)
+    console.log("Transition Image:", transitionImage)
+    let transitionImageExists = fs.existsSync(transitionImage)
+    if (transitionImageExists) {
+        console.log("Transition image exists")
+    } else {
+        console.log("Transition image does not exist")
+    }
 }
 module.exports = {
     renderTransition
