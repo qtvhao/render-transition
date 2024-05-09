686bb7b5537c0b0b7729da943fae6896715afe9a
Your Name <you@example.com>
2024-05-09 16:39:27 +0000

    Refactor image paths in app.js and docker-compose.yml

--- src/renderTransition.js
+++ src/renderTransition.js
@@ -1,51 +1,51 @@
 let fs = require('fs');
-// let sharp = require('sharp');
+let sharp = require('sharp');
 function getRandomFontStyle() {
     var fontStyles = [
-      'MediumItalic', 'Italic', 'Light', 'BlackItalic', 'Alternates-BoldItalic', 'Alternates-Bold', 'Alternates-LightItalic', 'Alternates-MediumItalic', 'SemiBold', 'Thin', 'Alternates-ExtraBold', 'Black', 'Regular', 'SemiBoldItalic', 'Alternates-ExtraBoldItalic', 'Bold', 'ExtraBoldItalic', 'Alternates-ExtraLightItalic', 'Alternates-Black', 'Alternates-ThinItalic', 'Alternates-ExtraLight', 'Medium', 'ExtraBold', 'Alternates-SemiBold', 'Alternates-BlackItalic', 'LightItalic', 'Alternates-SemiBoldItalic', 'Alternates-Light', 'ExtraLight', 'ThinItalic', 'ExtraLightItalic', 'BoldItalic', 'Alternates-Thin', 'Alternates-Medium', 'Alternates-Regular', 'Alternates-Italic',
+        'MediumItalic', 'Italic', 'Light', 'BlackItalic', 'Alternates-BoldItalic', 'Alternates-Bold', 'Alternates-LightItalic', 'Alternates-MediumItalic', 'SemiBold', 'Thin', 'Alternates-ExtraBold', 'Black', 'Regular', 'SemiBoldItalic', 'Alternates-ExtraBoldItalic', 'Bold', 'ExtraBoldItalic', 'Alternates-ExtraLightItalic', 'Alternates-Black', 'Alternates-ThinItalic', 'Alternates-ExtraLight', 'Medium', 'ExtraBold', 'Alternates-SemiBold', 'Alternates-BlackItalic', 'LightItalic', 'Alternates-SemiBoldItalic', 'Alternates-Light', 'ExtraLight', 'ThinItalic', 'ExtraLightItalic', 'BoldItalic', 'Alternates-Thin', 'Alternates-Medium', 'Alternates-Regular', 'Alternates-Italic',
     ].filter(font => {
-      return !font.includes('Alternates') && !font.includes('Italic') && !font.includes('Light')
-    } )
-// 
-// Montserrat-MediumItalic.otf
-// Montserrat-Italic.otf
-// Montserrat-Light.otf
-// Montserrat-BlackItalic.otf
-// fonts.scale
-// MontserratAlternates-BoldItalic.otf
-// MontserratAlternates-Bold.otf
-// MontserratAlternates-LightItalic.otf
-// MontserratAlternates-MediumItalic.otf
-// Montserrat-SemiBold.otf
-// Montserrat-Thin.otf
-// MontserratAlternates-ExtraBold.otf
-// Montserrat-Black.otf
-// Montserrat-Regular.otf
-// Montserrat-SemiBoldItalic.otf
-// MontserratAlternates-ExtraBoldItalic.otf
-// Montserrat-Bold.otf
-// Montserrat-ExtraBoldItalic.otf
-// MontserratAlternates-ExtraLightItalic.otf
-// MontserratAlternates-Black.otf
-// MontserratAlternates-ThinItalic.otf
-// MontserratAlternates-ExtraLight.otf
-// Montserrat-Medium.otf
-// Montserrat-ExtraBold.otf
-// MontserratAlternates-SemiBold.otf
-// MontserratAlternates-BlackItalic.otf
-// Montserrat-LightItalic.otf
-// MontserratAlternates-SemiBoldItalic.otf
-// MontserratAlternates-Light.otf
-// Montserrat-ExtraLight.otf
-// Montserrat-ThinItalic.otf
-// Montserrat-ExtraLightItalic.otf
-// Montserrat-BoldItalic.otf
-// MontserratAlternates-Thin.otf
-// MontserratAlternates-Medium.otf
-// MontserratAlternates-Regular.otf
-// MontserratAlternates-Italic.otf
-// fonts.dir
-  
+        return !font.includes('Alternates') && !font.includes('Italic') && !font.includes('Light')
+    })
+    // 
+    // Montserrat-MediumItalic.otf
+    // Montserrat-Italic.otf
+    // Montserrat-Light.otf
+    // Montserrat-BlackItalic.otf
+    // fonts.scale
+    // MontserratAlternates-BoldItalic.otf
+    // MontserratAlternates-Bold.otf
+    // MontserratAlternates-LightItalic.otf
+    // MontserratAlternates-MediumItalic.otf
+    // Montserrat-SemiBold.otf
+    // Montserrat-Thin.otf
+    // MontserratAlternates-ExtraBold.otf
+    // Montserrat-Black.otf
+    // Montserrat-Regular.otf
+    // Montserrat-SemiBoldItalic.otf
+    // MontserratAlternates-ExtraBoldItalic.otf
+    // Montserrat-Bold.otf
+    // Montserrat-ExtraBoldItalic.otf
+    // MontserratAlternates-ExtraLightItalic.otf
+    // MontserratAlternates-Black.otf
+    // MontserratAlternates-ThinItalic.otf
+    // MontserratAlternates-ExtraLight.otf
+    // Montserrat-Medium.otf
+    // Montserrat-ExtraBold.otf
+    // MontserratAlternates-SemiBold.otf
+    // MontserratAlternates-BlackItalic.otf
+    // Montserrat-LightItalic.otf
+    // MontserratAlternates-SemiBoldItalic.otf
+    // MontserratAlternates-Light.otf
+    // Montserrat-ExtraLight.otf
+    // Montserrat-ThinItalic.otf
+    // Montserrat-ExtraLightItalic.otf
+    // Montserrat-BoldItalic.otf
+    // MontserratAlternates-Thin.otf
+    // MontserratAlternates-Medium.otf
+    // MontserratAlternates-Regular.otf
+    // MontserratAlternates-Italic.otf
+    // fonts.dir
+
     var fontStyle = fontStyles[Math.floor(Math.random() * fontStyles.length)];
     return "Montserrat-" + fontStyle;
 }
@@ -60,7 +60,7 @@ var vibrantDarkColors = [
     "7B1FA2",
     "880E4F",
     "9C27B0",
-  ];
+];
 
 let renderTransition = async (translated, transitionImage, id) => {
     let yearMonthDay = new Date().toISOString().split('T')[0] // 2021-08-01
@@ -83,42 +83,35 @@ let renderTransition = async (translated, transitionImage, id) => {
     let resizedTransitionImage = "/tmp/resized-transition-" + id + ".jpg"
     let innerText = "/tmp/inner-text-" + id + ".png"
     let innerTextBlurred = "/tmp/inner-text-blurred-" + id + ".png"
+    let compositeFile = '/tmp/composite-' + id + '.jpg'
     if (transitionImageExists) {
         console.log("Transition image exists")
-        let imaginaryService = process.env.DEBUG ? 'imaginary' : 'localhost'
+        let imaginaryImagesService = process.env.DEBUG ? 'imaginary:9001' : 'localhost:9001'
+        let imaginaryService = process.env.DEBUG ? 'imaginary-tmp' : 'localhost'
         console.log('Resizing transition image. File: ', transitionImage)
-        let fetched = await fetch(`http://${imaginaryService}:9000/resize?width=3840&height=2160&file=${transitionImage}`)
+        let fetched = await fetch(`http://${imaginaryImagesService}/resize?width=2560&height=1440&nocrop=false&file=${transitionImage.replace('/app/storage/images/', '')}`)
         let resized = await fetched.arrayBuffer()
         fs.writeFileSync(resizedTransitionImage, Buffer.from(resized));
         // await sharp(transitionImage)
         //     .resize(3840, 2160)
         //     .toFile(resizedTransitionImage)
         let text = '<span foreground="#' + color + '">' + translated + '</span>';
-        console.log('text: ' , text)
+        console.log('text: ', text)
         await sharp({
             text: {
                 text,
                 //   resolution is 4k
-                width: 3840 * 0.8,
-                height: 2160 * 0.8,
+                width: 2560 * 0.8,
+                height: 1440 * 0.8,
                 font,
                 rgba: true,
             }
         })
             .toFile(innerText)
         // add gaussian blur to the text
-        await sharp({
-            text: {
-                text: '<span foreground="#FFFFFF">' + translated + '</span>',
-                //   resolution is 4k
-                width: 3840 * 0.8,
-                height: 2160 * 0.8,
-                font,
-                rgba: true,
-            }
-        })
-            .blur(50)
-            .toFile(innerTextBlurred)
+        let fetchedBlurred = await fetch(`http://${imaginaryService}:9000/blur?sigma=50&file=${innerText.replace('/tmp/', '')}`)
+        let blurred = await fetchedBlurred.arrayBuffer()
+        fs.writeFileSync(innerTextBlurred, Buffer.from(blurred));
         await sharp(resizedTransitionImage)
             .composite([
                 {
@@ -130,7 +123,10 @@ let renderTransition = async (translated, transitionImage, id) => {
                     gravity: 'center'
                 }
             ])
-            .toFile(outputFile)
+            .toFile(compositeFile)
+        fs.copyFileSync(compositeFile, outputFile)
+
+        return outputFile;
     } else {
         console.log("Transition image does not exist")
     }
